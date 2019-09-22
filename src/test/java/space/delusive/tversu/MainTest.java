39734bda6a905097d5ea778972704a9663c9e7d5
Delusive- <zoncck@yandex.ru>
2019-09-22 17:37:11 +0300

    Добавил лицензию, добавил ссылочку на репозиторий в pom, добавил наиважнейший тест

---
+++ src/test/java/space/delusive/tversu/MainTest.java
@@ -0,0 +1,16 @@
+package space.delusive.tversu;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    @DisplayName("Very useful test")
+    void shouldWork() {
+        assertTrue(true);
+    }
+
+}
