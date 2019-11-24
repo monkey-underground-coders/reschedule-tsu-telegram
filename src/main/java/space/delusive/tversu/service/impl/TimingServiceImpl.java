d2c351d3f10389595b8c5936a9cb303526bb3830
Delusive- <zoncck@yandex.ru>
2019-11-24 12:58:48 +0300

    Fix getting similar cells
    If one lesson has not one teacher this lesson is duplicated.
    Now if this happens similar cells are removing exclude one
    and all teachers names merge into saved one.

--- src/main/java/space/delusive/tversu/service/impl/TimingServiceImpl.java
+++ src/main/java/space/delusive/tversu/service/impl/TimingServiceImpl.java
@@ -10,8 +10,7 @@ import space.delusive.tversu.service.FacultyService;
 import space.delusive.tversu.service.TimingService;
 import space.delusive.tversu.util.DateUtils;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -57,11 +56,44 @@ public class TimingServiceImpl implements TimingService {
     }
 
     private Stream<Cell> getLessonsOfDayAsStream(User user, DayOfWeek day, boolean isNextWeek) {
+        return getLessonsOfWeekAsStream(user, isNextWeek)
+                .filter(cell -> cell.getDayOfWeek() == day);
+    }
+
+    private Stream<Cell> getLessonsOfWeekAsStream(User user, boolean isNextWeek) {
         List<Cell> cells = cellRepository.getCells(user.getFaculty(), user.getGroup());
+        uniteSimilarCells(cells);
         WeekSign targetWeekSign = isNextWeek ? facultyService.getNextWeekSign(user.getFaculty()) : facultyService.getCurrentWeekSign(user.getFaculty());
         return cells.stream()
                 .filter(cell -> cell.getWeekSign() == targetWeekSign || cell.getWeekSign() == WeekSign.ANY)
-                .filter(cell -> cell.getDayOfWeek() == day)
                 .filter(cell -> cell.getSubgroup() == user.getSubgroup() || cell.getSubgroup() == 0);
+    }
+
+    // i think i need to simplify this one, but now i'm too lazy to do it :p
+    private void uniteSimilarCells(List<Cell> cells) {
+        List<Cell> unitedCells = new ArrayList<>();
+        for (Cell cell : cells) {
+            if (unitedCells.contains(cell)) continue;
+            List<Cell> similarCells = getSimilarCells(cell, cells);
+            if (!similarCells.isEmpty()) {
+                unitedCells.addAll(similarCells);
+                similarCells.forEach(cell1 -> cell.setTeacherName(cell.getTeacherName() + ", " + cell1.getTeacherName()));
+            }
+        }
+        cells.removeAll(unitedCells);
+    }
+
+    private List<Cell> getSimilarCells(Cell cell, List<Cell> allCells) {
+        List<Cell> similarCells = new ArrayList<>();
+        allCells.forEach(oneCell -> {
+                boolean isSimilar = cell.getStart().equals(oneCell.getStart()) &&
+                        cell.getDayOfWeek() == oneCell.getDayOfWeek() &&
+                        cell.getSubgroup() == oneCell.getSubgroup() &&
+                        cell.getWeekSign() == oneCell.getWeekSign() &&
+                        cell.getFullSubjectName().equals(oneCell.getFullSubjectName()) &&
+                        cell != oneCell;
+                if (isSimilar) similarCells.add(oneCell);
+        });
+        return similarCells;
     }
 }
